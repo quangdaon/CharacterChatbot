7498f88a854695691e62468914924ad43b58b643
Quangdao Nguyen <qnguyen@nvisia.com>
2024-01-04 09:09:42 -0600

    Connect with API

--- CharacterChatbot.Cli/Program.cs
+++ CharacterChatbot.Cli/Program.cs
@@ -7,27 +7,71 @@ using Microsoft.Extensions.Configuration;
 IConfiguration config = new ConfigurationBuilder()
   .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+  .AddJsonFile("appsettings.secrets.json", optional: true, reloadOnChange: true)
   .Build();
 
-var chatbotProfile = GetProfile("Penguin");
-var context = new ChatContext(chatbotProfile);
+var chatbotProfile = GetProfile();
+
+var secretKey = config.GetValue<string>("OpenAi:SecretKey");
+var context = new ChatContext(secretKey, "Quangdao Nguyen", chatbotProfile);
 
 Console.WriteLine("You are speaking to {0}.", chatbotProfile.DisplayName);
 Console.WriteLine();
 
-while (true)
+await DoMessageLoop();
+return;
+
+async Task DoMessageLoop()
 {
   Console.Write("You: ");
   var next = Console.ReadLine();
 
-  if (string.IsNullOrWhiteSpace(next)) break;
+  if (string.IsNullOrWhiteSpace(next)) return;
 
   var response = await context.Process(next);
 
   Console.WriteLine("{0}: {1}", chatbotProfile.DisplayName, response);
+
+  await DoMessageLoop();
 }
 
-ChatbotProfile GetProfile(string key)
+ChatbotProfile GetProfile()
 {
-  return config.GetSection($"ChatbotProfiles:{key}").Get<ChatbotProfile>();
+  var sections = config.GetSection("ChatbotProfiles").Get<Dictionary<string, ChatbotProfile>>();
+  var profiles = sections.Values.ToArray();
+
+  var i = 0;
+  foreach (var profile in profiles)
+  {
+    Console.WriteLine($"{i + 1}) {profile.DisplayName}");
+    i++;
+  }
+
+  Console.WriteLine();
+  Console.Write("Select a profile: ");
+
+  var selection = SelectProfile(profiles);
+
+  Console.WriteLine();
+  return selection;
+}
+
+ChatbotProfile SelectProfile(ChatbotProfile[] profiles)
+{
+  var input = Console.ReadLine();
+
+  if (string.IsNullOrWhiteSpace(input)) Environment.Exit(0);
+
+  try
+  {
+    var entry = int.Parse(input);
+    var index = entry - 1;
+
+    return profiles[index];
+  }
+  catch
+  {
+    Console.Write("Invalid input. Try again: ");
+    return SelectProfile(profiles);
+  }
 }
--- CharacterChatbot.Engine/ChatContext.cs
+++ CharacterChatbot.Engine/ChatContext.cs
@@ -1,18 +1,48 @@
 ﻿using CharacterChatbot.Engine.Configuration;
+using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
 
 namespace CharacterChatbot.Engine;
 
 public class ChatContext
 {
-  private readonly ChatbotProfile _profile;
+  private const string CONSTANT_PROMPT =
+    "Another employee at nvisia named {0} has opened a private Microsoft Teams chat with you. " +
+    "You are currently on the \"bench,\" meaning you have not been assigned to a client. You are not an AI assistant.";
+  private readonly OpenAIAPI _api;
+  private readonly List<ChatMessage> _messages;
 
-  public ChatContext(ChatbotProfile profile)
+  public ChatContext(string secretKey, string user, ChatbotProfile profile)
   {
-    _profile = profile;
+    _api = new OpenAIAPI(secretKey);
+    var prompt = $"{profile.Prompt} {string.Format(CONSTANT_PROMPT, user)}";
+    Console.WriteLine("ChatGPT Prompt: {0}", prompt);
+    Console.WriteLine();
+
+    _messages = new List<ChatMessage>
+    {
+      new(ChatMessageRole.System, prompt)
+    };
   }
 
-  public Task<string> Process(string next)
+  public async Task<string> Process(string next)
   {
-    return Task.FromResult($"My name is {_profile.DisplayName}.");
+    var userMessage = new ChatMessage(ChatMessageRole.User, next);
+    _messages.Add(userMessage);
+    
+    var chatResult = await _api.Chat.CreateChatCompletionAsync(new ChatRequest
+    {
+      Model = Model.ChatGPTTurbo,
+      Temperature = 0.1,
+      MaxTokens = 180,
+      Messages = _messages
+    });
+    
+    
+    var responseMessage = chatResult.Choices[0].Message;
+    _messages.Add(responseMessage);
+
+    return responseMessage.TextContent;
   }
 }
