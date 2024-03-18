996f6c5c4f9744d7202d17887800ea343dbe3189
Quangdao Nguyen <me@quangdao.com>
2024-03-17 20:49:53 -0500

    Set up Teams project

---
+++ CharacterChatbot.Teams/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+IConfiguration config = new ConfigurationBuilder()
+  .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+  .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+  .AddJsonFile("appsettings.secrets.json", optional: true, reloadOnChange: true)
+  .Build();
